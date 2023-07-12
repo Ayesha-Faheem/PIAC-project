38b013f125c903c3985a2502758f2595bc1e6a36
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:31 +0500

    Create ex19.ts

---
+++ assignment1/ex19.ts
@@ -0,0 +1,6 @@
+let guests = ["father", "mother", "grandparents"];
+let count = 0;
+for (; count < 3 ; count= count + 1 ) {
+    console.log("My dear ", guests[count] ," I would like to invite you to my house for dinner at 15th of july.")
+}
+console.log("I am inviting" , guests.length , "guests")
