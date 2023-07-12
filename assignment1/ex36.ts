34d97947d439475e2d3da56c6848cf8695e6b079
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:26 +0500

    Create ex36.ts

---
+++ assignment1/ex36.ts
@@ -0,0 +1,5 @@
+function make_shirt(size , message)  {
+    console.log("The size of the shirt is", size , ". The message printed on the shirt is", "\"" , message , "\"") ;
+     }
+
+ make_shirt("large","Life goes on") 
