8ea7160703f5669d95b0d7179b46eb414332bdd1
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:29 +0500

    Create ex37.ts

---
+++ assignment1/ex37.ts
@@ -0,0 +1,8 @@
+
+function make_shirt(size = "Large", message= "I love typescript")  {
+    console.log("The size of the shirt is", size , ". The message printed on the shirt is", "\"" , message , "\"") ;
+     }
+
+ make_shirt() 
+ make_shirt("medium")
+ make_shirt("small", "You r cool")
