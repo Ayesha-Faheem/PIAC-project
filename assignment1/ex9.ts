5a343cc5c9d3d1f640443780bb6ac63d01c088f6
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:33:57 +0500

    Create ex9.ts

---
+++ assignment1/ex9.ts
@@ -0,0 +1,5 @@
+//Ayesha Faheem
+//Date : 6 July 2023
+// In this program i am storing my favourite number in a variable and then printing a message using that number
+let fav_num= 3
+console.log("My birthday is on", fav_num , "november")
