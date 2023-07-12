2b4c056b3c52578c21e901a9506350c37cc85091
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:11 +0500

    Create ex12.ts

---
+++ assignment1/ex12.ts
@@ -0,0 +1,5 @@
+let friends = ["Aamna", "Areeba", "Hooriya", "Moiza"];
+let index = 0;
+for ( ; index < friends.length; index = index + 1) {
+    console.log("My best friend is " , friends[index]);
+}
