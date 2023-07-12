03fa55d91288cdcba763022b23f617c25b242656
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:58 +0500

    Create ex27.ts

---
+++ assignment1/ex27.ts
@@ -0,0 +1,27 @@
+//version 1
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("Player earned 5 points")
+}   else if (alien_color == "yellow") {
+    console.log("The player earned 10 points")
+}   else if (alien_color == "red") {
+    console.log("The player earned 15 points")
+}
+//version 2
+var alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("Player earned 5 points")
+}   else if (alien_color == "yellow") {
+    console.log("The player earned 10 points")
+}   else if (alien_color == "red") {
+    console.log("The player earned 15 points")
+}
+//version 3
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("Player earned 5 points")
+}   else if (alien_color == "yellow") {
+    console.log("The player earned 10 points")
+}   else if (alien_color == "red") {
+    console.log("The player earned 15 points")
+}
