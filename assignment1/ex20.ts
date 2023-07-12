9fa992bc489997afe1781703bb75431c6c277528
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:34 +0500

    Create ex20.ts

---
+++ assignment1/ex20.ts
@@ -0,0 +1,6 @@
+const subjects = ["Maths", "Physics", "Chemistry", "Computer", "Biology"]
+console.log(subjects[0])
+console.log(subjects[1])
+console.log(subjects[2])
+console.log(subjects[3])
+console.log(subjects[4])
