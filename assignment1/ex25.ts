c43206ce23819e29f0d2f192905cc209970091b6
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:52 +0500

    Create ex25.ts

---
+++ assignment1/ex25.ts
@@ -0,0 +1,8 @@
+var alien_color = "green";
+if (alien_color == "green"){
+    console.log("Player earned 5 points")
+}
+var alien_color = "yellow"
+if(alien_color == "green"){
+    console.log("Player earned 5 points")
+}
