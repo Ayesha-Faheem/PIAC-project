072905b6946b3e0570d4a2bded18548612cfe7a7
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:53 +0500

    Create ex44.ts

---
+++ assignment1/ex44.ts
@@ -0,0 +1,7 @@
+function sandwiches(...items){
+    console.log("The sandwich that is ordered should contain following items", items)
+
+}
+sandwiches("brown bread , patti , ketchup,mayonisse")
+sandwiches("white bread , chicken breast , mustard , lettuce")
+sandwiches("pita bread , chicken strips, jalapanoes , spiracha")
