7f931c616c443091bfec3c3001569333551ce51d
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:15 +0500

    Create ex13.ts

---
+++ assignment1/ex13.ts
@@ -0,0 +1,4 @@
+let transportation = ["car", "motorcycle", "airplane"];
+console.log("i want to drive tesla" , transportation[0]);
+console.log("i had ride kawasaki", transportation[1], "when i was in dubai");
+console.log("i had travelled in emrirate", transportation[2] );
