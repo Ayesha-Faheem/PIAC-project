6e49e09b151226c930de0f094dba962c4b9e62f0
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:47 +0500

    Create ex23.ts

---
+++ assignment1/ex23.ts
@@ -0,0 +1,35 @@
+let names = "Ayesha";
+let age = 17;
+let collegeName = "Cedar College";
+let education = "Alevels";
+let gender = "Female";
+// TRUE CONDITIONS
+console.log("Is name= Ayesha? I predict true",);
+console.log(names=="Ayesha");
+console.log("Is age = 17? I predict true")
+console.log(age==17)
+console.log("Is college name = Cedar COllege? I predict true")
+console.log(collegeName=="Cedar College")
+console.log("Is education = Alevels? I predict true")
+console.log(education=="Alevels")
+console.log("Is gender = Female? I predict true")
+console.log(gender=="Female")
+// FALSE CONDITIONS
+console.log("Is name= Fatima? I predict False",);
+console.log(names=="Fatima");
+console.log("Is age = 16? I predict False")
+console.log(age==16)
+console.log("Is college name = Nixor College ? I predict False")
+console.log(collegeName=="Nixor College")
+console.log("Is education = Olevels? I predict False")
+console.log(education=="Olevels")
+console.log("Is gender = male? I predict False")
+console.log(gender=="male")
+
+
+
+
+
+
+
+
