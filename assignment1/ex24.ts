dcb684d43c303f5b91c35e6688668fb15bb08257
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:49 +0500

    Create ex24.ts

---
+++ assignment1/ex24.ts
@@ -0,0 +1,26 @@
+let city = "KARACHI";
+//tests for string
+console.log( city=="KARACHI");
+console.log(city =="");
+//tests for lowercase function
+console.log("karachi" == city.toLowerCase());
+console.log(city.toLowerCase() == "KARACHI");
+//numerical tests
+console.log(3 == 3)
+console.log(3 == 13)
+console.log(4 != 40)
+console.log(3 != 3)
+console.log(8>3)
+console.log(8<3)
+console.log(8>=8)
+console.log(14<=50)
+//and or tests
+console.log(city="KARACHI" && 3==3)
+console.log(8 > 2 && 6 > 10)
+console.log( 9==9 || 8==7)
+console.log(8+8==12 || 8+8==11)
+//testing whether item is in array
+const names = ["ayesha", "fatima", "umaima"]
+console.log(names.includes("ayesha"))
+console.log(names.includes("maryam"))
+
