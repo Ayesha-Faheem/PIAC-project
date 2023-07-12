cbc4e6126ab9bcd00365fcf1ec52f90fc30d75a2
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:34:55 +0500

    Create ex26.ts

---
+++ assignment1/ex26.ts
@@ -0,0 +1,14 @@
+// version 1
+var alien_color1 = "yellow";
+if (alien_color1=="green" ) {
+    console.log("The player just earned 5 points for shooting the alien.");}
+else {
+    console.log("The player just earned 10 points");
+}
+// version 2
+var alien_color2 = "green";
+if (alien_color2=="green" ) {
+    console.log("The player just earned 5 points for shooting the alien.");}
+else {
+    console.log("The player just earned 10 points");
+}
