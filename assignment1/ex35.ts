09635a967c69dae7c1c21bbe779af299f85c0e77
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:23 +0500

    Create ex35.ts

---
+++ assignment1/ex35.ts
@@ -0,0 +1,6 @@
+const animals = ["parrot" , "cat", "dog"]
+let index1 = 0
+for (; index1 < 3 ; index1 = index1 +1) {
+    console.log("A" , animals[index1], "would make a great pet")
+}
+console.log("All of these three animals would make a great pet")
