5999bb521f42e14f333a8d1900403599abda5da0
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:42 +0500

    Create ex41.ts

---
+++ assignment1/ex41.ts
@@ -0,0 +1,11 @@
+const magicians= ["triumph", "lance", "criss"]
+
+function show_magicians(array) {
+    let index = 0
+    for (; index < array.length; index = index + 1) {
+      console.log(array[index]);
+    }
+  }
+
+show_magicians(magicians)
+
