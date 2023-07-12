ed63b32d1e25f952b759397f3175d23d16a18662
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:32 +0500

    Create ex38.ts

---
+++ assignment1/ex38.ts
@@ -0,0 +1,7 @@
+function describe_city(city , country= "Pakistan")  {
+    console.log(city, "is in", country) ;
+     }
+
+ describe_city("islamabad")
+ describe_city("dubai", "UAE")
+ describe_city("karachi") 
