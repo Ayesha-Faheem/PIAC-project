4d4e352dc18255d7eda17c3bef6870099f204350
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:04 +0500

    Create ex29.ts

---
+++ assignment1/ex29.ts
@@ -0,0 +1,16 @@
+const favorite_fruits = ["mango", "watermelon", "peach"]
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes!")
+}
+if (favorite_fruits.includes("banabao")) {
+    console.log("You really like bananas!")
+}
+if (favorite_fruits.includes("watermelon")) {
+    console.log("You really like watermelons!")
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!")
+}
+if (favorite_fruits.includes("peach")) {
+    console.log("You really like peach!")
+}
