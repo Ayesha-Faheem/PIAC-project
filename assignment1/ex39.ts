99990b111bfc14737255a28fa3071ccd8c5f94eb
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:35 +0500

    Create ex39.ts

---
+++ assignment1/ex39.ts
@@ -0,0 +1,7 @@
+function city_country(city , country)  {
+    let message = "\"" + city + "," + country + "\""
+    return message ;
+     }
+console.log(city_country("Lahore" , "Pakistan"))
+console.log(city_country("Istanbul" , "Turkey"))
+console.log(city_country("Makkah" , "Saudia Arabia"))
