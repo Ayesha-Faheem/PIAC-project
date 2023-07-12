e0b87945679e2db15c571fc6931d83af17dc6477
Computrrr <ayesha.faheem2005@gmail.com>
2023-07-12 21:35:19 +0500

    Create ex34.ts

---
+++ assignment1/ex34.ts
@@ -0,0 +1,6 @@
+const pizza = ["Fajita pizza", "Ranch pizza", "Pepperoni pizza"];
+let index = 0
+for (; index < 3 ; index = index +1) {
+    console.log("I like" , pizza[index])
+}
+console.log("I really love pizza")
